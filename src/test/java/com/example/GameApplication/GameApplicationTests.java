6b9a7b3f3d48e5d81f61e83fdae1c8aecbc33ef1
Hilal <beyzaparlakci@gmail.com>
2024-07-30 18:07:52 +0300

    First commit

---
+++ src/test/java/com/example/GameApplication/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GameApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
